9b630687e3f960d4426e035595e99f472f58bee9
VinhNguyenUte <valtriek99@gmail.com>
2025-03-06 19:18:23 +0700

    lan 3

--- BTOOP_CCB/BTOOP_CCB/Program.cs
+++ BTOOP_CCB/BTOOP_CCB/Program.cs
@@ -4,13 +4,13 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        IEmployee employee1 = new PartTimeEmployee("Trung", 45000, 7);
-        Console.WriteLine("Name: " + employee1.GetName());
-        Console.WriteLine("Salary per day: " + employee1.CalculateSalary());
+        IEmployee BIEN_2233_1 = new PartTimeEmployee("HUYNH NGUYEN VINH NGUYEN - 2233 PART TIME", 45000, 7);
+        Console.WriteLine("Name: " + BIEN_2233_1.GetName());
+        Console.WriteLine("Salary per day: " + BIEN_2233_1.CalculateSalary());
 
-        IEmployee employee2 = new FullTimeEmployee("Linh", 65000);
-        Console.WriteLine("Name: " + employee2.GetName());
-        Console.WriteLine("Salary per day: " + employee2.CalculateSalary());
+        IEmployee BIEN_2233_2 = new FullTimeEmployee("HUYNH NGUYEN VINH NGUYEN - 2233 FULL TIME", 65000);
+        Console.WriteLine("Name: " + BIEN_2233_2.GetName());
+        Console.WriteLine("Salary per day: " + BIEN_2233_2.CalculateSalary());
         Console.ReadKey();
     }
 }
--- BTOOP_CCB/BTOOP_CCB/obj/Debug/net8.0/BTOOP_CCB.AssemblyInfo.cs
+++ BTOOP_CCB/BTOOP_CCB/obj/Debug/net8.0/BTOOP_CCB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTOOP_CCB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c5784fafefd56157c572d53f219b2a544f43b4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+334060a27f2d37bb8e6ad2f194280de198f77cc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTOOP_CCB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTOOP_CCB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
